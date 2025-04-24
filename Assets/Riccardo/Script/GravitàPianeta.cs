c709df1c9303f7e552e832ef8e5e238edd9550c8
Rhisen93 <antoniocaprino@kromix.it>
2025-04-24 23:55:32 +0200

    Modifiche allo Script GravitàPianeta

--- Assets/Riccardo/Script/GravitàPianeta.cs
+++ Assets/Riccardo/Script/GravitàPianeta.cs
@@ -4,19 +4,21 @@ public class GravitàPianeta : MonoBehaviour
 {
     public float gravitàPianeta = 2f;
     public Transform[] oggettiConGravitàPianeta;
+    private float raggioPianeta = 10f;
 
     private void Gravità()
     {
         Vector3 gravitàCustomOggetto = Vector3.zero;
         RaycastHit hit;
+        
 
         foreach (Transform oggetto in oggettiConGravitàPianeta)
         {
-            gravitàCustomOggetto = (oggetto.position - this.transform.position).normalized * gravitàPianeta * Time.fixedDeltaTime;
-            if (Physics.Linecast(oggetto.position, gravitàCustomOggetto, out hit))
+            gravitàCustomOggetto = (oggetto.position - this.transform.position).normalized * gravitàPianeta * Time.fixedDeltaTime * -1f;
+            if (Physics.Linecast(oggetto.position, oggetto.position + gravitàCustomOggetto, out hit))
                 oggetto.position = hit.point;
             else
-                oggetto.position += gravitàCustomOggetto;
+                oggetto.position += gravitàCustomOggetto + gravitàCustomOggetto * 0.1f;
         }
     }
 
